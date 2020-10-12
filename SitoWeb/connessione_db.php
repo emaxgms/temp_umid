b630fa37e5f0c01473f560f1cb8c136e6e2f2a04
Emanuele Vacca <emaxgms@gmail.com>
2020-10-12 16:51:35 +0200

    Update connessione_db.php

--- SitoWeb/connessione_db.php
+++ SitoWeb/connessione_db.php
@@ -1,5 +1,5 @@
 <?php
-	$c=mysqli_connect('localhost','root','');
+	$c=mysqli_connect('localhost','USERNAME_DB','PASSWORD_DB');
 	if (!$c) {
 		die("Connection failed: " . mysqli_connect_error());
 	}
